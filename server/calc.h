0da00726b5f9476a7f553a2aa86263191aa6875a
ivakhnoff <vanya.shurmanov@yandex.ru>
2024-12-12 13:09:07 +0300

    Update calc.h

--- server/calc.h
+++ server/calc.h
@@ -2,15 +2,16 @@
 #include <vector>
 #include <iostream>
 #include <limits>
-#include <stdlib.h>
+#include <stdexcept> // Для исключений
+#include <boost/numeric/conversion/cast.hpp> // Для проверки на переполнение
 
 class calc {
 public:
-    uint32_t res; 
+    uint32_t res; // Результат вычислений
 
-
+    // Конструктор
     calc(std::vector<uint32_t> chisla);
 
-   
+    // Метод для получения результата
     uint32_t send_res();
 };
